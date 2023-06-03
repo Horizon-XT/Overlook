bca71be0dc627890f19f67ea10acafb7243fed69
OnofreTZK <taraujonofre@gmail.com>
2023-06-02 22:44:45 -0300

    first commit

---
+++ lib/overlook.ex
@@ -0,0 +1,20 @@
+defmodule Overlook do
+  @moduledoc """
+  Documentation for `Overlook`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Overlook.hello()
+      :world
+
+  """
+  def hello do
+    :world
+
+    IO.puts("Hello Danny! Wanna Play with us?")
+  end
+end
---
+++ test/overlook_test.exs
@@ -0,0 +1,8 @@
+defmodule OverlookTest do
+  use ExUnit.Case
+  doctest Overlook
+
+  test "greets the world" do
+    assert Overlook.hello() == :world
+  end
+end
